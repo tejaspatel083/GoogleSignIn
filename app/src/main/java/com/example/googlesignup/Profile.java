5f3582fba83e450a6bc52b7bc950f99b48bdef73
TejasPatel <tjp083@gmail.com>
2021-06-16 18:36:38 -0400

    Update-2

--- app/src/main/java/com/example/googlesignup/Profile.java
+++ app/src/main/java/com/example/googlesignup/Profile.java
@@ -6,16 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
 
 public class Profile extends AppCompatActivity {
 
     TextView name, mail;
     Button logout;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +28,14 @@ public class Profile extends AppCompatActivity {
         logout = findViewById(R.id.logout);
         name = findViewById(R.id.name);
         mail = findViewById(R.id.mail);
+        img = findViewById(R.id.imageView);
 
 
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
         if(signInAccount != null){
             name.setText(signInAccount.getDisplayName());
             mail.setText(signInAccount.getEmail());
+            Picasso.get().load(signInAccount.getPhotoUrl()).into(img);
         }
 
 
